e0beb692b6e2e12b3dd34bdd3589cb779b00a1f3
Matthew Quaglia <matthewquaglia@Studio-MBP.local>
2026-04-11 13:25:21 +0930

    remove base path now custom domain is live

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.empire-eyes.com",
-  base: process.env.GITHUB_ACTIONS ? "/empire-eyes-website" : "/",
   integrations: [sitemap()],
 });
